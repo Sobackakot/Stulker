52ade974db4a55e6fa8caa0fa2e08db8a9a40794
Aibulat <119883177+Sobackakot@users.noreply.github.com>
2024-11-04 00:57:47 +0300

    fix

--- Assets/Scripts/InventorySystem/Inventory/InventorySlot.cs
+++ Assets/Scripts/InventorySystem/Inventory/InventorySlot.cs
@@ -32,27 +32,18 @@ public class InventorySlot : MonoBehaviour, IDropHandler
     {
         ItemInSlot dropItem = eventData.pointerDrag.GetComponent<ItemInSlot>(); 
         ItemScrObj itemData = dropItem.dataItem; 
-        
-        if (transformSlot.childCount > 0 && itemData != null)
-        {
-            if (!CheckDropItemType(dropItem)) return;
-            DropItemInventory(itemData, dropItem); 
-        }
+        if (transformSlot.childCount > 0 && itemData != null) DropItemInventory(itemData, dropItem);
     }
     private void DropItemInventory(ItemScrObj itemData, ItemInSlot dropItem)
-    {    
-        ItemInSlot pickItem = transformSlot.GetChild(0).GetComponent<ItemInSlot>(); 
-        ItemScrObj oldItemData = inventoryController.SwapItemFromInventory(itemData, pickItem.slotIndex); 
-
-        if (oldItemData != null)
-        {
-            inventoryController.SwapItemFromInventory(oldItemData, dropItem.slotIndex); 
-        }
-    } 
-    private bool CheckDropItemType(ItemInSlot dropItem)
-    {   
-        InventorySlot dropSlot = dropItem.originalSlot;
+    {
         ItemInSlot pickItem = transformSlot.GetChild(0).GetComponent<ItemInSlot>();
+        if (!CheckDropItemType(dropItem, pickItem)) return;
+        ItemScrObj oldItemData = inventoryController.SwapItemFromInventory(itemData, pickItem.slotIndex); 
+        if (oldItemData != null) inventoryController.SwapItemFromInventory(oldItemData, dropItem.slotIndex);
+    } 
+    private bool CheckDropItemType(ItemInSlot dropItem, ItemInSlot pickItem)
+    {   
+        InventorySlot dropSlot = dropItem.originalSlot; 
         if(dropSlot.gameObject.tag == "FastSlot") return false;
         if(dropSlot.gameObject.tag == "EquipSlot" && pickItem.dataItem != null) return false; 
         if(dropSlot.gameObject.tag == "EquipSlot" && pickItem.dataItem == null)
@@ -65,7 +56,6 @@ public class InventorySlot : MonoBehaviour, IDropHandler
     private void UnEquip(ItemInSlot dropItem)
     {
         inventoryController.UpdateEquip(dropItem.dataItem, out ItemScrObj oldItem);
-        equipmentController.RemoveItemFromInventory(dropItem.dataItem);
-        if (oldItem != null) Debug.Log("Bug drop Equip!!!-------------------------------------------------------");
+        equipmentController.RemoveItemFromInventory(dropItem.dataItem); 
     }
 }
