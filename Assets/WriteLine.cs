0cfb92fb9ac95b45f8b42bfa29b0135642bd58a4
Aibulat <119883177+Sobackakot@users.noreply.github.com>
2025-01-01 22:34:50 +0300

    AddReadLineAnime

--- Assets/WriteLine.cs
+++ Assets/WriteLine.cs
@@ -4,12 +4,28 @@ using UnityEngine;
 public class WriteLine : MonoBehaviour
 {
     [SerializeField] private Transform target;
-    [SerializeField] private Transform handRight; 
+    [SerializeField] private Transform handRight;
+
+    [SerializeField] private bool isLocal = false;
+    private Vector3 localPos;
+    private Vector3 localRot;
+  
 
     private void LateUpdate()
     {
-        handRight.position = target.position;
-        handRight.rotation = target.rotation;
+        if (!isLocal)
+        {
+            handRight.position = target.position;
+            handRight.rotation = target.rotation;
+        }
+        else
+        { 
+                localPos = target.localPosition;
+            handRight.localPosition = localPos; 
+                localRot = target.localEulerAngles;
+            handRight.localEulerAngles = localRot;
+        }
+        
     }
 
 }
