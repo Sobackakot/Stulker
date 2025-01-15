630b8026f16c480ee1754df29974774609b494d6
Aibulat <119883177+Sobackakot@users.noreply.github.com>
2025-01-15 14:43:21 +0300

    save

--- Assets/Scripts/TEST/ItemDataCast.cs
+++
@@ -1,9 +0,0 @@
- 
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "itemCast", menuName = "Inventt/item")]
-public class ItemDataCast : ScriptableObject
-{
-    public string nameItem;
-    public Sprite image; 
-}
